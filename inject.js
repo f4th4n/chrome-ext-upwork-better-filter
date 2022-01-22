3debc3797b6d4814b194f01c2d77dc5f9b15aaf8
Wildan Fathan <wilfathan@gmail.com>
2022-01-22 17:24:45 +0700

    Version 2

--- inject.js
+++ inject.js
@@ -1,9 +1,11 @@
-var blacklistCountry = ["India", "Nigeria", "Bangladesh", "Pakistan", "Nepal"];
+var blacklistCountry = [];
 
 function removeBadJobs() {
   document.querySelectorAll("strong.text-muted").forEach((el) => {
     if (el.innerText === "Payment unverified") {
-      el.closest(".up-card-section").remove();
+      if (el.closest(".up-card-section")) {
+        el.closest(".up-card-section").remove();
+      }
     }
   });
 
@@ -11,9 +13,26 @@ function removeBadJobs() {
     .querySelectorAll('[data-test="client-country"] strong')
     .forEach((el) => {
       if (blacklistCountry.includes(el.innerText)) {
-        el.closest(".up-card-section").remove();
+        if (el.closest(".up-card-section")) {
+          el.closest(".up-card-section").remove();
+        }
       }
     });
+
+  // https://www.upwork.com/ab/jobs/search
+  document.querySelectorAll(".glyphicon.air-icon-unverified").forEach((el) => {
+    if (el.closest("section.job-tile")) {
+      el.closest("section.job-tile").remove();
+    }
+  });
+
+  document.querySelectorAll("strong.client-location").forEach((el) => {
+    if (blacklistCountry.includes(el.innerText)) {
+      if (el.closest("section.job-tile")) {
+        el.closest("section.job-tile").remove();
+      }
+    }
+  });
 }
 
 setInterval(() => {
